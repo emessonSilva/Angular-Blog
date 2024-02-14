bfb2c56c4d19510d640591373c86a1ba38a46439
Emesson <emessonbarbosa600@gmail.com>
2024-02-14 00:07:24 -0300

    🖌️Dinamic components and responsive sheets added🖌️

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css', './big-card.responsive.component.css']
 })
 export class BigCardComponent implements OnInit {
+
+  //Deixando dinâmico
+  @Input()
+  photoCover:string = "" //coloca no html home
+  @Input()
+  cardTitle:string = "" //coloca no html home
+  @Input()
+  cardDescription:string = "" //coloca no html home
 
   constructor() { }
 
