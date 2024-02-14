d033a6d0f7d3f3ee0994dd7895ebc62edda3430f
Emesson <emessonbarbosa600@gmail.com>
2024-02-14 20:35:55 -0300

    🖌️Fake API created and connected🖌️

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "photo",
+        "title": "title",
+        "description": "description",
+        "photoCover": "https://www.morecap.com.br/assets/images/SM-placeholder.png"
+    },
+    {
+        "id": "2",
+        "photo": "photo",
+        "title": "title",
+        "description": "description",
+        "photoCover": "https://www.morecap.com.br/assets/images/SM-placeholder.png"
+    }
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -9,16 +10,29 @@ import { ActivatedRoute } from '@angular/router';
 export class ContentComponent implements OnInit {
 
 
-  photoCover:string = "https://www.morecap.com.br/assets/images/SM-placeholder.png"
-  contentTitle:string = "Title"
-  contentDescription:string = "Descrpition"
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
+
+  private id:string | null = "0"
 
   constructor( private route:ActivatedRoute ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+
+  this.contentTitle = result.title    
+  this.contentDescription = result.description
+  this.photoCover = result.photoCover
+
   }
 
 }
