2ec2c090d9a07b7c5c48114f34985bb36ed68210
Emesson <emessonbarbosa600@gmail.com>
2024-02-21 01:35:52 -0300

    🖌️About me and one projetct added🖌️

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,13 +1,42 @@
 export const dataFake = [
     {
         "id": "1",
+
+        "photo": "./assets/images/Foto.jpeg",
+
+        "title": "About me",
+
+        "description": `My name is Emesson Silva, I'm 21 years old and I currently live in the city of Recife - PE.
+        I finished high school in 2022 at IFPE - Campus Recife, there I also completed the building technician course.
+        I'm currently doing a degree in I.T. with the systems analysis and development course at Faculdade Senac-PE,
+        on the night shift and I'm in the 3rd period. In Jan/2024 I completed the FAP (Accelerated Training in Programming)
+        in the Front-End part provided by Softex and there I learnt about web development. At the moment I am looking to enter the I.T. area (internship, CLT,...)
+        in the development or data area.`,                
+        
+        "photoCover": "./assets/images/Foto.jpeg"
+    },
+    {
+        "id": "2",
+
+        "photo": "photo",
+
+        "title": "Spotify Page",
+
+        "description": `This project is a web interface created from the Spotify website. To create this interface, I used the following 
+        technologies: HTML, CSS, and JavaScript. The Alura front-end immersion, which lasted 5 days, allowed me to 
+        accomplish this project. I have made the project available on my GitHub, through the following link: https://github.com/emessonSilva/Spotify-Page .`,
+
+        "photoCover": "./assets/images/SpotifyPage.png"
+    },
+    {
+        "id": "3",
         "photo": "photo",
         "title": "title",
         "description": "description",
         "photoCover": "https://www.morecap.com.br/assets/images/SM-placeholder.png"
     },
     {
-        "id": "2",
+        "id": "4",
         "photo": "photo",
         "title": "title",
         "description": "description",
