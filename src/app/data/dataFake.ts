e1755f526ef48ca9b7c779b975eb47dca845249c
Emesson <emessonbarbosa600@gmail.com>
2024-02-27 01:16:06 -0300

    🖌️Database and Style changed🖌️

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,8 +2,6 @@ export const dataFake = [
     {
         "id": "1",
 
-        "photo": "./assets/images/Foto.jpeg",
-
         "title": "About me",
 
         "description": `My name is Emesson Silva, I'm 21 years old and I currently live in the city of Recife - PE.
@@ -18,8 +16,6 @@ export const dataFake = [
     {
         "id": "2",
 
-        "photo": "photo",
-
         "title": "Spotify Page",
 
         "description": `This project is a web interface created from the Spotify website. To create this interface, I used the following 
@@ -30,16 +26,25 @@ export const dataFake = [
     },
     {
         "id": "3",
-        "photo": "photo",
-        "title": "title",
-        "description": "description",
-        "photoCover": "https://www.morecap.com.br/assets/images/SM-placeholder.png"
+
+        "title": "BuzzFeed Game",
+
+        "description": `This Angular project is a game based on BuzzFeed tests, the theme is about The Avengers.
+        It implements an interactive quiz to determine which Avenger you would be. Using a main component, it loads questions and options from a JSON database. 
+        Users are guided through the questions, make choices, and their answers are collected. At the end, the system calculates the result based on the user's choices, consulting 
+        the database to determine the corresponding Avenger character. The result is displayed along with an image associated with the chosen Avenger. The project stands out for its 
+        integration of JSON data, interactivity, and dynamism provided by the Angular framework, offering an engaging and personalized user experience. 
+        You can access this project through the following link: https://github.com/emessonSilva/BuzzFeed-Game .`,
+
+        "photoCover": "./assets/images/BuzzFeed.png"
     },
     {
         "id": "4",
-        "photo": "photo",
+
         "title": "title",
+
         "description": "description",
+
         "photoCover": "https://www.morecap.com.br/assets/images/SM-placeholder.png"
     }
 ]
